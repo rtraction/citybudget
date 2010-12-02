912c69994e12ffe3a619c7987bcafe5f12917f74
Zoe Blair <zoster.i@gmail.com>
2010-12-02 00:06:11 -0500

    changed the structure of the API controller

--- system/application/controllers/api.php
+++ system/application/controllers/api.php
@@ -12,12 +12,51 @@ class Api extends Controller {
 		$this->load->view('welcome_message');
 	}
 	
-	function organizations($format = 'json'){
-		$data = array();
-		
-		$data['data'] = $this->Budget->get_organizations();
-		
+	/* 
+	 * RESPONSE
+	 * -Year
+	 *   -Org1
+	 * 	   -Program
+	 * 	     -Number Proposed
+	 * 	     -Number Approved
+	 *       -Grouping (if exists)
+	 *     -Program2
+	 *       -...
+	 *   -Org2 
+	 *     -...
+	 *
+	 */
+	function budget($year = 0, $orgs = '',/* $program = '',*/ $format = 'json'){
 		$data['format'] = $format;
+		
+		if((int)$year == 0){
+			//load a view that explains how to use this API call
+			
+			
+		}else{
+			//$data['data'] = $this->Budget->get_
+			$this->load->view('api', $data);
+		}
+	}
+	
+	function listing($year='', $item='', $format = 'json'){
+		$data['format'] = $format;
+		
+		//If no valid year is printed, show some instructions for this function.
+		if((int)$year == 0){
+			//load a view that explains how to use this API call
+		}else{
+	 		switch($item){
+				case 'organizations':
+					$data['data'] = $this->Budget->get_organizations();
+					break;
+				default:
+					//show all available budget years
+					
+					
+
+			}
+		}
 		$this->load->view('api', $data);
 	}
 }
--- system/application/views/api.php
+++ system/application/views/api.php
@@ -1,5 +1,9 @@
-﻿<?php
+<?php
 switch($format) {
+	case 'xml':
+		//echo xml_encode(); <-- wouldn't that be nice?
+		//break;
+	case 'json':
 	default:
 		echo json_encode($data);
 	break;
