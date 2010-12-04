f760facfa580b6024c0e597a576696663284a987
Greg Smith <greg.smith@rtraction.com>
2010-12-04 12:48:15 -0800

    switched to jquery for server-side ajax

--- readJSON.js
+++ readJSON.js
@@ -1,20 +1,3 @@
-var xhr;
-
 function readJSON(url, callback) {	 
-	try {
-	  xhr = new ActiveXObject("Microsoft.XMLHTTP");    // Trying Internet Explorer 
-	}
-	catch(e)    // Failed 
-	{
-	  xhr = new XMLHttpRequest();    // Other browsers.
-	}
-		
-	try {		
-		xhr.open("GET", url, true);
-		xhr.onreadystatechange = callback;
-		xhr.send(null);
-	}
-	catch(err) {
-		return "failed";
-	}
+	$.getJSON(url, callback(json));
 }
