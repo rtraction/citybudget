bffe73c396eb22ad0d34d8545c8360e24b6f3e52
Gavin Blair <gavin.blair@rtraction.com>
2010-12-04 01:10:16 -0500

    removing old CI stuff. Updated readme

--- system/logs/log-2010-12-04.php
+++ system/logs/log-2010-12-04.php
@@ -42,3 +42,37 @@ DEBUG - 2010-12-04 01:38:31 --> Helper loaded: url_helper
 DEBUG - 2010-12-04 01:38:31 --> Database Driver Class Initialized
 DEBUG - 2010-12-04 01:38:31 --> Model Class Initialized
 DEBUG - 2010-12-04 01:38:31 --> Controller Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Config Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Hooks Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> URI Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> No URI present. Default controller set.
+DEBUG - 2010-12-04 00:01:38 --> Router Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Output Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Input Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Global POST and COOKIE data sanitized
+DEBUG - 2010-12-04 00:01:38 --> Language Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Loader Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Helper loaded: url_helper
+DEBUG - 2010-12-04 00:01:38 --> Database Driver Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Model Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> Controller Class Initialized
+DEBUG - 2010-12-04 00:01:38 --> File loaded: application/views/welcome_message.php
+DEBUG - 2010-12-04 00:01:38 --> Final output sent to browser
+DEBUG - 2010-12-04 00:01:38 --> Total execution time: 0.2375
+DEBUG - 2010-12-04 00:01:39 --> Config Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Hooks Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> URI Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> No URI present. Default controller set.
+DEBUG - 2010-12-04 00:01:39 --> Router Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Output Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Input Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Global POST and COOKIE data sanitized
+DEBUG - 2010-12-04 00:01:39 --> Language Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Loader Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Helper loaded: url_helper
+DEBUG - 2010-12-04 00:01:39 --> Database Driver Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Model Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> Controller Class Initialized
+DEBUG - 2010-12-04 00:01:39 --> File loaded: application/views/welcome_message.php
+DEBUG - 2010-12-04 00:01:39 --> Final output sent to browser
+DEBUG - 2010-12-04 00:01:39 --> Total execution time: 0.2365
--- system_old/application/views/footer.php
+++
@@ -1,4 +0,0 @@
-<p><br />Page rendered in {elapsed_time} seconds</p>
-
-</body>
-</html>
