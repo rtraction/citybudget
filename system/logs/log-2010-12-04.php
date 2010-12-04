e4352b43e681c4f4e631f1800ffd5d12039459f5
root <root@ubuntu.(none)>
2010-12-04 01:41:43 -0600

    using phil sturgeon's restful code igniter install. going to follow the tut at net.tutsplus.com/tutorials/php/working-with-restful-services-in-codeigniter-2/

---
+++ system/logs/log-2010-12-04.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2010-12-04 01:37:28 --> Config Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Hooks Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> URI Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> No URI present. Default controller set.
+DEBUG - 2010-12-04 01:37:28 --> Router Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Output Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Input Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Global POST and COOKIE data sanitized
+DEBUG - 2010-12-04 01:37:28 --> Language Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Loader Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Helper loaded: url_helper
+DEBUG - 2010-12-04 01:37:28 --> Database Driver Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> Controller Class Initialized
+DEBUG - 2010-12-04 01:37:28 --> File loaded: application/views/welcome_message.php
+DEBUG - 2010-12-04 01:37:28 --> Final output sent to browser
+DEBUG - 2010-12-04 01:37:28 --> Total execution time: 0.1953
+DEBUG - 2010-12-04 01:37:41 --> Config Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Hooks Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> URI Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Router Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Output Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Input Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Global POST and COOKIE data sanitized
+DEBUG - 2010-12-04 01:37:41 --> Language Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Loader Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Helper loaded: url_helper
+DEBUG - 2010-12-04 01:37:41 --> Database Driver Class Initialized
+DEBUG - 2010-12-04 01:37:41 --> Controller Class Initialized
+ERROR - 2010-12-04 01:37:41 --> Severity: Notice  --> Undefined property: Api::$Budget /var/www/citybudgetapi/application/controllers/api.php 36
+DEBUG - 2010-12-04 01:38:31 --> Config Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Hooks Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> URI Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Router Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Output Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Input Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2010-12-04 01:38:31 --> Language Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Loader Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Helper loaded: url_helper
+DEBUG - 2010-12-04 01:38:31 --> Database Driver Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Model Class Initialized
+DEBUG - 2010-12-04 01:38:31 --> Controller Class Initialized
